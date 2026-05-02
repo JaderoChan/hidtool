fe8ada6d008cdee5dace1b27b0b9f557ec94712f
JaderoChan <c_dl_cn@outlook.com>
2026-05-02 16:27:28 +0800

    Update VirtualScreenInfo comment

--- src/mouse/win/virtual_screen_info.hpp
+++ src/mouse/win/virtual_screen_info.hpp
@@ -6,6 +6,10 @@
 namespace hidtool
 {
 
+// 使用 int16_t 作为成员变量类型，能够使得结构体总大小为8字节，以便其能在最少依赖的情况下保证可原子化。
+// （Windows 下超过8字节的平凡数据类型需要额外依赖/实现才能保证其可原子化）
+// 此外，由于虚拟屏幕的坐标实际上均由16位有符号值表示，故不存在数值溢出等问题。
+// （参见 https://learn.microsoft.com/en-us/windows/win32/gdi/the-virtual-screen）
 struct VirtualScreenInfo
 {
     static VirtualScreenInfo getLatest();
