fe235d9f95262951eefa0a2336c31017ff77f095
JaderoChan <c_dl_cn@outlook.com>
2026-05-02 16:18:33 +0800

    Update circle time

--- example/mouse_circle/main.cpp
+++ example/mouse_circle/main.cpp
@@ -23,7 +23,7 @@ int main(int argc, char* argv[])
         int32_t x = halfWidth + halfWidth * cos(th * PI / 180.0);
         int32_t y = halfHeight + halfHeight * sin(th * PI / 180.0);
         msSim.moveTo({x, y});
-        std::this_thread::sleep_for(std::chrono::microseconds(200));
+        std::this_thread::sleep_for(std::chrono::microseconds(100));
         th += 0.1;
     }
 
