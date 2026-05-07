9b7c7239f622599f03d390db65f51dfe37ce80b9
JaderoChan <c_dl_cn@outlook.com>
2026-05-08 00:28:40 +0800

    Update comments

--- include/hidtool/mouse/mouse_event.hpp
+++ include/hidtool/mouse/mouse_event.hpp
@@ -17,8 +17,8 @@ struct MouseEvent
         ET_WHEEL,
         /**
          * @details 在 **Windows** 和 **Linux** 平台下，此事件永远不会被 `MouseHooker` 的事件处理程序接收到。
-         * 当在上述两个平台下发送此类型的事件时，其等同于绝对移动事件与鼠标按键事件的组合。在 **MacOS** 平台下，
-         * 无论是 `MouseHooker` 还是 `MouseSimulator`，都原生支持此类型事件。
+         * 当在上述两个平台下发送此类型的事件时，其等同于绝对移动事件与鼠标按键事件的组合，字段 `dragButton` 将被丢弃。
+         * 在 **MacOS** 平台下，无论是 `MouseHooker` 还是 `MouseSimulator`，都原生支持此类型事件。
          */
         ET_DRAG,
         ET_PRESS,
--- include/hidtool/mouse/mouse_simulator.hpp
+++ include/hidtool/mouse/mouse_simulator.hpp
@@ -77,7 +77,8 @@ public:
     /**
      * @brief 拖拽鼠标。
      * @attention 不包含鼠标按键函数。
-     * @details 在 **Windows** 和 **Linux** 平台下，等同于 \ref `moveTo()`。在 **MacOS** 平台下，其原生支持。
+     * @details 在 **Windows** 和 **Linux** 平台下，等同于 \ref `moveTo()`，参数 `button` 将被丢弃。
+     * 在 **MacOS** 平台下，其原生支持。
      * @sa \ref mouse_drag_combo
      */
     bool dragTo(const AbsolutePos& absPos, MouseButton button = MSBTN_LEFT);
