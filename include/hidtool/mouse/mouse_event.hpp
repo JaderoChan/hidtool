5a7ddfe4b092951cc4326e44fd51b5b84f4700b2
JaderoChan <c_dl_cn@outlook.com>
2026-05-08 22:24:26 +0800

    Update comment

--- include/hidtool/mouse/mouse_event.hpp
+++ include/hidtool/mouse/mouse_event.hpp
@@ -21,7 +21,7 @@ struct MouseEvent
         ET_WHEEL,
         /**
          * @details 在 **Windows** 和 **Linux** 平台下，此事件永远不会被 `MouseHooker` 的事件处理程序接收到。
-         * 当在上述两个平台下发送此类型的事件时，其等同于绝对移动事件与鼠标按键事件的组合，字段 `drag.button` 将被丢弃。
+         * 当在上述两个平台下发送此类型的事件时，其等同于绝对移动事件，字段 `drag.button` 将被丢弃。
          * 在 **MacOS** 平台下，无论是 `MouseHooker` 还是 `MouseSimulator`，都原生支持此类型事件。
          */
         ET_DRAG,
